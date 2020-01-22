67f768912c31bc06e233d6a1314c0d8e84b0cebf
Martin Parkin <github@memia.biz>
2020-01-23 00:18:05 +0400

    Added DateTime extensions

---
+++ MPL.Common.UnitTests/DateTimeExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPL.Common.TestHelpers;
+using System;
+
+namespace MPL.Common
+{
+    [TestClass]
+    public class DateTimeExtensionsTests
+    {
+        [TestMethod]
+        public void DateTimeExtensions_ToIso8601_Exists()
+        {
+            Assert.IsTrue(ExtensionMethodTestHelper.ContainsNamedExtensionMethod(typeof(DateTime), typeof(DateTimeExtensions), nameof(DateTimeExtensions.ToIso8601)));
+        }
+
+        [TestMethod]
+        public void DateTimeExtensions_ToIso8601Z_Exists()
+        {
+            Assert.IsTrue(ExtensionMethodTestHelper.ContainsNamedExtensionMethod(typeof(DateTime), typeof(DateTimeExtensions), nameof(DateTimeExtensions.ToIso8601Z)));
+        }
+
+        [TestMethod]
+        public void Conversion_ToIso8601_OutputCorrect()
+        {
+            DateTime dateTime = new DateTime(2020, 01, 25, 15, 58, 22);
+            Assert.AreEqual("2020-01-25T15:58:22", dateTime.ToIso8601());
+        }
+
+        [TestMethod]
+        public void Conversion_ToIso8601Z_OutputCorrect()
+        {
+            DateTime dateTime = new DateTime(2020, 01, 25, 15, 58, 22);
+            Assert.AreEqual("2020-01-25T15:58:22Z", dateTime.ToIso8601Z());
+        }
+    }
+}
---
+++ MPL.Common/DateTimeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MPL.Common
+{
+    /// <summary>
+    /// A class that defines extensions to DateTime.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        #region Methods
+        #region _Public_
+        /// <summary>
+        /// Gets the specified datetime as an ISO 8601 string.
+        /// </summary>
+        /// <param name="dateTime">A DateTime that is the date to convert.</param>
+        /// <returns>A string containing the date and time in ISO 8601 format.</returns>
+        public static string ToIso8601(this DateTime dateTime)
+        {
+            return dateTime.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Gets the specified datetime as an ISO 8601 string with a Zulu timezone indicator.
+        /// </summary>
+        /// <param name="dateTime">A DateTime that is the date to convert.</param>
+        /// <returns>A string containing the date and time in ISO 8601 format.</returns>
+        public static string ToIso8601Z(this DateTime dateTime)
+        {
+            return dateTime.ToIso8601() + "Z";
+        }
+
+        #endregion
+        #endregion
+    }
+}
