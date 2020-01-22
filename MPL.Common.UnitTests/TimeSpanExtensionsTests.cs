999b0550dc1958f249020eaf3817b01b460ee36e
Martin Parkin <github@memia.biz>
2020-01-22 12:55:29 +0000

    Added TimeSpan extension methods

---
+++ MPL.Common.UnitTests/TimeSpanExtensionsTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPL.Common.TestHelpers;
+using System;
+
+namespace MPL.Common
+{
+    [TestClass]
+    public class TimeSpanExtensionsTests
+    {
+        [TestMethod]
+        public void TimeSpanExtensions_ToTextualTimestamp_Exists()
+        {
+            Assert.IsTrue(ExtensionMethodTestHelper.ContainsNamedExtensionMethod(typeof(TimeSpan), typeof(TimeSpanExtensions), nameof(TimeSpanExtensions.ToTextualTimestamp)));
+            Assert.IsTrue(ExtensionMethodTestHelper.ContainsNamedExtensionMethod(typeof(TimeSpan), typeof(TimeSpanExtensions), nameof(TimeSpanExtensions.ToTextualTimestamp), new[] { typeof(bool), typeof(bool) }));
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_DefaultDHMSM_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(20, 19, 18, 17, 16);
+            Assert.AreEqual("20 days, 19 hours, 18 minutes and 17 seconds", timeSpan.ToTextualTimestamp());
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_DefaulDHMSMtWithAllZeroes_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(0, 0, 0, 0, 0);
+            Assert.AreEqual(string.Empty, timeSpan.ToTextualTimestamp());
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_DefaultDHMSMWithOnes_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(1, 1, 1, 1, 1);
+            Assert.AreEqual("1 day, 1 hour, 1 minute and 1 second", timeSpan.ToTextualTimestamp());
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_DefaultDHMSMWithZeroes_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(1, 0, 1, 0, 1);
+            Assert.AreEqual("1 day and 1 minute", timeSpan.ToTextualTimestamp());
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_WithMillisecondsDHMSM_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(20, 19, 18, 17, 16);
+            Assert.AreEqual("20 days, 19 hours, 18 minutes, 17 seconds and 16 milliseconds", timeSpan.ToTextualTimestamp(true, false));
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_WithMillisecondsDHMSMWithAllZeroes_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(0, 0, 0, 0, 0);
+            Assert.AreEqual(string.Empty, timeSpan.ToTextualTimestamp(true, false));
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_WithMillisecondsDHMSMWithOnes_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(1, 1, 1, 1, 1);
+            Assert.AreEqual("1 day, 1 hour, 1 minute, 1 second and 1 millisecond", timeSpan.ToTextualTimestamp(true, false));
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_WithMillisecondsDHMSMWithZeroes_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(1, 0, 1, 0, 1);
+            Assert.AreEqual("1 day, 1 minute and 1 millisecond", timeSpan.ToTextualTimestamp(true, false));
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_WithZeroesDHMSM_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(20, 0, 18, 0, 16);
+            Assert.AreEqual("20 days, 0 hours, 18 minutes, 0 seconds and 16 milliseconds", timeSpan.ToTextualTimestamp(true, true));
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_WithZeroesDHMSMtWithAllZeroes_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(0, 0, 0, 0, 0);
+            Assert.AreEqual("0 days, 0 hours, 0 minutes, 0 seconds and 0 milliseconds", timeSpan.ToTextualTimestamp(true, true));
+        }
+
+        [TestMethod]
+        public void ToTextualTimestamp_WithZeroesDHMSMWithZeroes_OutputCorrect()
+        {
+            TimeSpan timeSpan = new TimeSpan(1, 0, 1, 0, 1);
+            Assert.AreEqual("1 day, 0 hours, 1 minute, 0 seconds and 1 millisecond", timeSpan.ToTextualTimestamp(true, true));
+        }
+    }
+}
---
+++ MPL.Common/TimeSpanExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace MPL.Common
+{
+    /// <summary>
+    /// A class that defines extensions to a TimeSpan.
+    /// </summary>
+    public static class TimeSpanExtensions
+    {
+        #region Methods
+        #region _Private_
+        private static bool ValueToOutput(int value, string name, string plural, bool outputZeroValues, out string output)
+        {
+            output = string.Empty;
+            bool returnValue = false;
+
+            if (value > 0)
+            {
+                if (value == 1)
+                    output = $"1 {name}";
+                else
+                    output = $"{value} {plural}";
+            }
+            else
+            {
+                if (outputZeroValues)
+                    output = $"0 {plural}";
+            }
+
+            if (!string.IsNullOrWhiteSpace(output))
+                returnValue = true;
+
+            return returnValue;
+        }
+
+        #endregion
+        #region _Public_
+        /// <summary>
+        /// Gets a textual respresentation of the specified TimeSpan without milliseconds or zero value outputs.
+        /// </summary>
+        /// <param name="timeSpan">A TimeSpan to get the textual representation of.</param>
+        /// <returns>A string containing the textual representation.</returns>
+        public static string ToTextualTimestamp(this TimeSpan timeSpan)
+        {
+            return ToTextualTimestamp(timeSpan, false, false);
+        }
+        /// <summary>
+        /// Gets a textual respresentation of the specified TimeSpan.
+        /// </summary>
+        /// <param name="timeSpan">A TimeSpan to get the textual representation of.</param>
+        /// <param name="outputZeroValues">A bool indicating whether to output zero values.</param>
+        /// <param name="outputMilliseconds">A bool indicating whether to output millseconds.</param>
+        /// <returns>A string containing the textual representation.</returns>
+        public static string ToTextualTimestamp(this TimeSpan timeSpan, bool outputMilliseconds, bool outputZeroValues)
+        {
+            int counter = 0;
+            string[] elements = new string[5];
+            string returnValue = string.Empty;
+
+            if (ValueToOutput(timeSpan.Days, "day", "days", outputZeroValues, out string output))
+                elements[counter++] = output;
+            if (ValueToOutput(timeSpan.Hours, "hour", "hours", outputZeroValues, out output))
+                elements[counter++] = output;
+            if (ValueToOutput(timeSpan.Minutes, "minute", "minutes", outputZeroValues, out output))
+                elements[counter++] = output;
+            if (ValueToOutput(timeSpan.Seconds, "second", "seconds", outputZeroValues, out output))
+                elements[counter++] = output;
+            if (outputMilliseconds && ValueToOutput(timeSpan.Milliseconds, "millisecond", "milliseconds", outputZeroValues, out output))
+                elements[counter++] = output;
+
+            for (int i = 0; i < counter; i++)
+            {
+                if (i != 0)
+                {
+                    if (i == counter - 1)
+                    {
+                        returnValue += $" and {elements[i]}";
+                    }
+                    else
+                    {
+                        returnValue += $", {elements[i]}";
+                    }
+                }
+                else
+                    returnValue = elements[i];
+            }
+
+            return returnValue;
+        }
+
+        #endregion
+        #endregion
+    }
+}
