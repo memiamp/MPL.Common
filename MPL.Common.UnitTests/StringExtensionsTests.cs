8f87b2b36559ca0e8b8ecb702d60323fffe2d273
memiamap <github@memia.biz>
2021-05-17 19:19:20 +0100

    Introduced StringExtensions and associated tests

---
+++ MPL.Common.UnitTests/StringExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPL.Common.TestHelpers;
+using System;
+
+namespace MPL.Common
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void CountAllChar_EmptyString_ReturnsZero()
+        {
+            Assert.AreEqual("".CountAll('A'), 0);
+        }
+
+        [TestMethod]
+        public void CountAllChar_MultiMatch_ReturnsMulti()
+        {
+            Assert.AreEqual("ABCDEABCDEABCDEABCDEABCDE".CountAll('A'), 5);
+        }
+
+        [TestMethod]
+        public void CountAllChar_NoMatches_ReturnsZero()
+        {
+            Assert.AreEqual("ABCDEFGHIJKLMNOPQRSTUVWXY".CountAll('Z'), 0);
+        }
+
+        [TestMethod]
+        public void CountAllChar_NullString_ReturnsZero()
+        {
+            string testString = null;
+
+            Assert.AreEqual(testString.CountAll('A'), 0);
+        }
+
+        [TestMethod]
+        public void CountAllChar_OneMatch_ReturnsOne()
+        {
+            Assert.AreEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ".CountAll('A'), 1);
+        }
+
+
+        [TestMethod]
+        public void CountAllString_EmptyString_ReturnsZero()
+        {
+            Assert.AreEqual("".CountAll("AB"), 0);
+        }
+
+        [TestMethod]
+        public void CountAllString_MultiMatch_ReturnsMulti()
+        {
+            Assert.AreEqual("ABCDEABCDEABCDEABCDEABCDE".CountAll("BCD"), 5);
+        }
+
+        [TestMethod]
+        public void CountAllString_NoMatches_ReturnsZero()
+        {
+            Assert.AreEqual("ABCDEFGHIJKLMNOPQRSTUVWXY".CountAll("AZ"), 0);
+        }
+
+        [TestMethod]
+        public void CountAllString_NullString_ReturnsZero()
+        {
+            string testString = null;
+
+            Assert.AreEqual(testString.CountAll("AB"), 0);
+        }
+
+        [TestMethod]
+        public void CountAllString_OneMatch_ReturnsOne()
+        {
+            Assert.AreEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ".CountAll("IJKL"), 1);
+        }
+
+        [TestMethod]
+        public void StringExtensions_CountAllChar_Exists()
+        {
+            Assert.IsTrue(ExtensionMethodTestHelper.ContainsNamedExtensionMethod(typeof(string), typeof(StringExtensions), nameof(StringExtensions.CountAll), new[] { typeof(char) }));
+        }
+
+        [TestMethod]
+        public void StringExtensions_CountAllString_Exists()
+        {
+            Assert.IsTrue(ExtensionMethodTestHelper.ContainsNamedExtensionMethod(typeof(string), typeof(StringExtensions), nameof(StringExtensions.CountAll), new[] { typeof(string) }));
+        }
+    }
+}
---
+++ MPL.Common/StringExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MPL.Common
+{
+    /// <summary>
+    /// A class that defines extension methods for a string.
+    /// </summary>
+    public static class StringExtensions
+    {
+        #region Methods
+        #region _Public_
+        /// <summary>
+        /// Counts all occurrences of the specified char in a string.
+        /// </summary>
+        /// <param name="haystack">A string that is the string to count occurrences in.</param>
+        /// <param name="needle">A char that is the occurence to count.</param>
+        /// <returns>An int indicating the number of occurrences.</returns>
+        public static int CountAll(this string haystack, char needle)
+        {
+            int returnValue = 0;
+
+            if (haystack != null)
+            {
+                int len = haystack.Length;
+
+                for (int i = 0; i < len; i++)
+                    if (haystack[i] == needle)
+                        returnValue++;
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Counts all occurrences of the specified string in a string.
+        /// </summary>
+        /// <param name="haystack">A string that is the string to count occurrences in.</param>
+        /// <param name="needle">A string that is the occurence to count.</param>
+        /// <returns>An int indicating the number of occurrences.</returns>
+        public static int CountAll(this string haystack, string needle)
+        {
+            int returnValue = 0;
+
+            if (haystack != null)
+                returnValue = (haystack.Length - haystack.Replace(needle, string.Empty).Length) / needle.Length;
+
+            return returnValue;
+        }
+
+        #endregion
+        #endregion
+    }
+}
