974fab2d745973e59684e1b9aad6167167d96345
memiamap <github@memia.biz>
2021-07-29 22:02:16 +0100

    Introduced serializer options to allow more precision when serialisation and deserialisation occurs

---
+++ MPL.Common/Runtime/Serialization/SerializerOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MPL.Common.Runtime.Serialization
+{
+    /// <summary>
+    /// A class that defines options for the serializer.
+    /// </summary>
+    public class SerializerOptions
+    {
+        /// <summary>
+        /// Creates a new instance of the class.
+        /// </summary>
+        public SerializerOptions()
+            : this(null, false)
+        { }
+
+        /// <summary>
+        /// Creates a new instance of the class with the specified parameters.
+        /// </summary>
+        /// <param name="dateTimeFormat">A DateTimeFormat indicating the format to use when serialising DateTime types</param>
+        /// <param name="emitTypeInformation">A bool that indicates whether to emit type information for custom types.</param>
+        public SerializerOptions(DateTimeFormat dateTimeFormat, bool emitTypeInformation)
+        {
+            DateTimeFormat = dateTimeFormat;
+            EmitTypeInformation = emitTypeInformation;
+        }
+
+        /// <summary>
+        /// Gets or sets the format to use when serialisiing DateTime types.
+        /// </summary>
+        public DateTimeFormat DateTimeFormat { get; set; }
+
+        /// <summary>
+        /// Gets or sets an indication of whether to emit type information.
+        /// </summary>
+        public bool EmitTypeInformation { get; set; }
+    }
+}
--- MPL.Common/Runtime/Serialization/Serializer`1.cs
+++ MPL.Common/Runtime/Serialization/Serializer`1.cs
@@ -19,6 +19,7 @@ namespace MPL.Common.Runtime.Serialization
         /// </summary>
         static Serializer()
         {
+            _defaultSerializerOptions = new SerializerOptions();
             _Encoding = Encoding.Default;
         }
 
@@ -26,6 +27,7 @@ namespace MPL.Common.Runtime.Serialization
 
         #region Declarations
         #region _Members_
+        private static readonly SerializerOptions _defaultSerializerOptions;
         private static Encoding _Encoding;
 
         #endregion
@@ -34,14 +36,24 @@ namespace MPL.Common.Runtime.Serialization
         #region Methods
         #region _Public_
         /// <summary>
-        /// Deserialises the specified data.
+        /// Deserialises the specified data using the default deserialisation options.
         /// </summary>
         /// <param name="data">An array of byte containing the data to deserialise.</param>
         /// <returns>A T desrialised from the data.</returns>
         public static T Deserialise(byte[] data)
         {
+            return Deserialise(data, _defaultSerializerOptions);
+        }
+        /// <summary>
+        /// Deserialises the specified data.
+        /// </summary>
+        /// <param name="data">An array of byte containing the data to deserialise.</param>
+        /// <param name="serializerOptions">A SerializerOptions containing the serialiser options to use.</param>
+        /// <returns>A T desrialised from the data.</returns>
+        public static T Deserialise(byte[] data, SerializerOptions serializerOptions)
+        {
             MemoryStream Data;
-            T ReturnValue = default(T);
+            T ReturnValue = default;
 
             // Create the stream
             Data = new MemoryStream(data);
@@ -49,7 +61,7 @@ namespace MPL.Common.Runtime.Serialization
             try
             {
                 // Try to deserialise the data
-                ReturnValue = Deserialise(Data);
+                ReturnValue = Deserialise(Data, serializerOptions);
             }
             finally
             {
@@ -60,44 +72,75 @@ namespace MPL.Common.Runtime.Serialization
             return ReturnValue;
         }
         /// <summary>
-        /// Deserialises the specified data stream.
+        /// Deserialises the specified data stream using the default deserialisation options.
         /// </summary>
         /// <param name="data">A Stream containing the data to deserialise.</param>
         /// <returns>A T desrialised from the data.</returns>
         public static T Deserialise(Stream data)
         {
+            return Deserialise(data, _defaultSerializerOptions);
+        }
+        /// <summary>
+        /// Deserialises the specified data stream.
+        /// </summary>
+        /// <param name="data">A Stream containing the data to deserialise.</param>
+        /// <param name="serializerOptions">A SerializerOptions containing the serialiser options to use.</param>
+        /// <returns>A T desrialised from the data.</returns>
+        public static T Deserialise(Stream data, SerializerOptions serializerOptions)
+        {
             object SourceObject;
             DataContractJsonSerializer Serialiser;
-            T ReturnValue = default(T);
+            T ReturnValue = default;
 
             // Create the serialiser
-            Serialiser = CreateSerialiser();
+            Serialiser = CreateSerialiser(serializerOptions);
 
             // Desrialise the object and check it's valid
             SourceObject = Serialiser.ReadObject(data);
-            if (SourceObject != null && SourceObject is T)
-                ReturnValue = (T)SourceObject;
+            if (SourceObject != null && SourceObject is T castSourceObject)
+                ReturnValue = castSourceObject;
 
             return ReturnValue;
         }
         /// <summary>
-        /// Deserialises the specified string data using the default text encoding.
+        /// Deserialises the specified string data using the default text encoding and the default deserialisation options.
         /// </summary>
         /// <param name="data">A string containing the data to deserialise.</param>
         /// <returns>A T desrialised from the data.</returns>
         public static T Deserialise(string data)
         {
-            return Deserialise(data, _Encoding);
+            return Deserialise(data, _defaultSerializerOptions);
         }
         /// <summary>
-        /// Deserialises the specified string data using the specified text encoding.
+        /// Deserialises the specified string data using the default text encoding.
+        /// </summary>
+        /// <param name="data">A string containing the data to deserialise.</param>
+        /// <param name="serializerOptions">A SerializerOptions containing the serialiser options to use.</param>
+        /// <returns>A T desrialised from the data.</returns>
+        public static T Deserialise(string data, SerializerOptions serializerOptions)
+        {
+            return Deserialise(data, _Encoding, serializerOptions);
+        }
+        /// <summary>
+        /// Deserialises the specified string data using the specified text encoding and the default deserialisation options.
         /// </summary>
         /// <param name="data">A string containing the data to deserialise.</param>
         /// <param name="encoding">An Encoding that specifies the text encoding to use during desrialisation.</param>
         /// <returns>A T desrialised from the data.</returns>
         public static T Deserialise(string data, Encoding encoding)
         {
-            T ReturnValue = default(T);
+            return Deserialise(data, encoding, _defaultSerializerOptions);
+        }
+        /// <summary>
+        /// Deserialises the specified string data using the specified text encoding.
+        /// </summary>
+        /// <param name="data">A string containing the data to deserialise.</param>
+        /// <param name="encoding">An Encoding that specifies the text encoding to use during desrialisation.</param>
+        /// <param name="serializerOptions">A SerializerOptions containing the serialiser options to use.</param>
+        /// <returns>A T desrialised from the data.</returns>
+        public static T Deserialise(string data, Encoding encoding, SerializerOptions serializerOptions)
+        {
+            T ReturnValue = default;
 
             if (data != null)
             {
@@ -105,25 +148,35 @@ namespace MPL.Common.Runtime.Serialization
 
                 // Convert the string to a byte array and deserialise
                 Data = encoding.GetBytes(data);
-                ReturnValue = Deserialise(Data);
+                ReturnValue = Deserialise(Data, serializerOptions);
             }
 
             return ReturnValue;
         }
 
         /// <summary>
-        /// Serialises the specified object.
+        /// Serialises the specified object using the default serialisation options.
         /// </summary>
         /// <param name="obj">A T that is the object to serialise.</param>
         /// <returns>An array of byte containing the serialised object.</returns>
         public static byte[] Serialise(T obj)
         {
+            return Serialise(obj, _defaultSerializerOptions);
+        }
+        /// <summary>
+        /// Serialises the specified object.
+        /// </summary>
+        /// <param name="obj">A T that is the object to serialise.</param>
+        /// <param name="serializerOptions">A SerializerOptions containing the serialiser options to use.</param>
+        /// <returns>An array of byte containing the serialised object.</returns>
+        public static byte[] Serialise(T obj, SerializerOptions serializerOptions)
+        {
             DataContractJsonSerializer Serialiser;
-            byte[] ReturnValue = null;
+            byte[] ReturnValue;
             MemoryStream TargetStream;
 
             // Create the serialiser
-            Serialiser = CreateSerialiser();
+            Serialiser = CreateSerialiser(serializerOptions);
 
             // Serialise the object
             TargetStream = new MemoryStream();
@@ -134,27 +187,48 @@ namespace MPL.Common.Runtime.Serialization
         }
 
         /// <summary>
-        /// Serialises the specified object.
+        /// Serialises the specified object using the default encoding and the default serialisation options.
         /// </summary>
         /// <param name="obj">A T that is the object to serialise.</param>
         /// <returns>A string containing the serialised object.</returns>
         public static string SerialiseToString(T obj)
         {
-            return SerialiseToString(obj, _Encoding);
+            return SerialiseToString(obj, _defaultSerializerOptions);
         }
         /// <summary>
-        /// Serialises the specified object.
+        /// Serialises the specified object using the default encoding.
+        /// </summary>
+        /// <param name="obj">A T that is the object to serialise.</param>
+        /// <param name="serializerOptions">A SerializerOptions containing the serialiser options to use.</param>
+        /// <returns>A string containing the serialised object.</returns>
+        public static string SerialiseToString(T obj, SerializerOptions serializerOptions)
+        {
+            return SerialiseToString(obj, _Encoding, serializerOptions);
+        }
+        /// <summary>
+        /// Serialises the specified object using the default serialisation options.
         /// </summary>
         /// <param name="obj">A T that is the object to serialise.</param>
         /// <param name="encoding">An Encoding that specifies the text encoding to use during desrialisation.</param>
         /// <returns>A string containing the serialised object.</returns>
         public static string SerialiseToString(T obj, Encoding encoding)
         {
+            return SerialiseToString(obj, encoding, _defaultSerializerOptions);
+        }
+        /// <summary>
+        /// Serialises the specified object.
+        /// </summary>
+        /// <param name="obj">A T that is the object to serialise.</param>
+        /// <param name="encoding">An Encoding that specifies the text encoding to use during desrialisation.</param>
+        /// <param name="serializerOptions">A SerializerOptions containing the serialiser options to use.</param>
+        /// <returns>A string containing the serialised object.</returns>
+        public static string SerialiseToString(T obj, Encoding encoding, SerializerOptions serializerOptions)
+        {
             byte[] Data;
             string ReturnValue = null;
 
             // Get the serialised object and convert to text
-            Data = Serialise(obj);
+            Data = Serialise(obj, serializerOptions);
             if (Data != null && Data.Length > 0)
             {
                 ReturnValue = encoding.GetString(Data);
@@ -166,13 +240,24 @@ namespace MPL.Common.Runtime.Serialization
 
         #endregion
         #region _Private_
-        private static DataContractJsonSerializer CreateSerialiser()
+        private static DataContractJsonSerializer CreateSerialiser(SerializerOptions serializerOptions)
         {
-            DataContractJsonSerializerSettings Settings = new DataContractJsonSerializerSettings
+            return new DataContractJsonSerializer(typeof(T), MapSettings(serializerOptions));
+        }
+
+        private static DataContractJsonSerializerSettings MapSettings(SerializerOptions serializerOptions)
+        {
+            DataContractJsonSerializerSettings returnValue;
+
+            returnValue = new DataContractJsonSerializerSettings()
             {
-                EmitTypeInformation = EmitTypeInformation.Never
+                EmitTypeInformation = serializerOptions.EmitTypeInformation ? EmitTypeInformation.Always : EmitTypeInformation.Never
             };
-            return new DataContractJsonSerializer(typeof(T), Settings);
+
+            if (serializerOptions.DateTimeFormat != null)
+                returnValue.DateTimeFormat = serializerOptions.DateTimeFormat;
+
+            return returnValue;
         }
 
         #endregion
@@ -188,7 +273,7 @@ namespace MPL.Common.Runtime.Serialization
             set
             {
                 if (value == null)
-                    throw new ArgumentException("The specified encoding is null", "value");
+                    throw new ArgumentException("The specified encoding is null", nameof(value));
                 else
                     _Encoding = value;
             }
